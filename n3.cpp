956e5722843aa9b6dab9605abcd04b7e755befb1
SofiaM27 <moskalenkos.22560@gmail.com>
2025-10-13 16:15:26 +0300

    Add files via upload
    MoskalenkoSofia/homework1

---
+++ n3.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Comp on 13.10.2025.
+#include <stdio.h>
+
+int main() {
+    int a;
+
+    printf("Введіть цифру a: ");
+    scanf("%d", &a);
+
+    printf(" - %d - %d - %d\n", a, a, a);
+    printf(" %d | %d | %d\n", a, a, a);
+    printf(" - %d - %d - %d\n", a, a, a);
+
+    return 0;
+}
